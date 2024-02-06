fb7d79aa77668247b4489533b7ecf86cbe7ca8f1
Danylo Yenbaiev <ennnbaev@icloud.com>
2024-02-06 16:20:05 +0200

    Homework 11 Search in array

--- js/app.js
+++ js/app.js
@@ -1,39 +1,58 @@
-//Task 1 Hello User
-
-let name = prompt('What is your name?')
-alert(`Hello, ${name}! How are you?`)
-
-//Task 2 Lines and five-digit number
-
-let firstLine = prompt('Hello, please write down first line')
-let secondLine = prompt('Please write down second line')
-let thirdLine = prompt('Please write down third line')
-alert('Result of concat: ' + firstLine + secondLine + thirdLine)
-
-let number = prompt('Hello, please write down a five-digit number')
-alert(number.charAt(0) + ' ' + number.charAt(1) + ' ' + number.charAt(2) + ' ' + number.charAt(3) + ' ' + number.charAt(4)) //or we can use dividing 10 and operator %
-
-//Task 3 Calculator
-
-let operation = prompt('Hello, I am a super fast Calculator! Please chose operation  (add, sub, mult, div)')
-let firstNumb = prompt('Great! Write down a first number')
-let secondNumb = prompt('Cool! Write down a second number')
-if (operation === 'add') {
-    alert('Sum is: ' + (parseInt(firstNumb) + parseInt(secondNumb)))
-} else if (operation === 'sub') {
-    alert('Sub is: ' + (parseInt(firstNumb) - parseInt(secondNumb)))
-
-} else if (operation === 'mult') {
-    alert('Mult is: ' + (parseInt(firstNumb) * parseInt(secondNumb)))
-} else if (operation === 'div') {
-    alert('Div is: ' + (parseInt(firstNumb) / parseInt(secondNumb)))
-} else {
-    alert('Unsupported operation, please reload page and chose a correct operation (add, sub, mult, div)')
+let users = [{
+    index: 0,
+    isActive: true,
+    balance: 2226.60,
+    name: "Eugenia Sawyer",
+    gender: "female",
+    phone: "+1 (840) 583-3207",
+    address: "949 John Street, Rose, Puerto Rico, 1857"
+}, {
+    index: 1,
+    isActive: true,
+    balance: 2613.77,
+    name: "Pauline Gallegos",
+    gender: "female",
+    phone: "+1 (985) 593-3328",
+    address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+}, {
+    index: 2,
+    isActive: false,
+    balance: 3976.41,
+    name: "Middleton Chaney",
+    gender: "male",
+    phone: "+1 (995) 591-2478",
+    address: "807 Fleet Walk, Brutus, Arkansas, 9783"
+}, {
+    index: 3,
+    isActive: true,
+    balance: 1934.58,
+    name: "Burns Poole",
+    gender: "male",
+    phone: "+1 (885) 559-3422",
+    address: "730 Seba Avenue, Osage, Alabama, 6290"
+}, {
+    index: 4,
+    isActive: true,
+    balance: 3261.65,
+    name: "Mcfadden Horne",
+    gender: "male",
+    phone: "+1 (942) 565-3988",
+    address: "120 Scholes Street, Kirk, Michigan, 1018"
+}, {
+    index: 5,
+    isActive: false,
+    balance: 1790.56,
+    name: "Suzette Lewis",
+    gender: "female",
+    phone: "+1 (837) 586-3283",
+    address: "314 Dunne Place, Bawcomville, Guam, 9053"
+}]
+let usersPhoneWithBalanceMore2000 = [];
+let sumUsersBalance = 0;
+for (let i = 0; i < users.length; i++) {
+    if (users[i].balance > 2000) {
+        usersPhoneWithBalanceMore2000.push(users[i].phone);
+    }
+    sumUsersBalance += users[i].balance;
 }
-
-//Task 4 Hours in seconds
-
-let hours = prompt('Hey, it calculator from hours to second! Put humber of hours')
-const seconds = hours * 60 * 60
-alert(hours + ` hours equal - ${seconds} sec`)
-
+alert(`Users phone whose balance is more than 2000 ${usersPhoneWithBalanceMore2000} . The sum of users balance is ${sumUsersBalance}`);
