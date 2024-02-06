7e790111bbc3247dd9d480d635a733538cf88658
Danylo Yenbaiev <ennnbaev@icloud.com>
2024-02-06 23:09:01 +0200

    Homework 10 Improved code style

--- js/app10.js
+++ js/app10.js
@@ -1,7 +1,7 @@
 const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 let indexOfMexElement = 0;
 let maxValue = array[0];
-for (let i = 1; i < array.length; i++) {
+for (let i = 0; i < array.length; i++) {
     if (array[i] > maxValue) {
         maxValue = array[i];
         indexOfMexElement = i;
