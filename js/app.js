a0927dc055359dbbd0fa8005b3c07b0cca4373ac
Danylo Yenbaiev <ennnbaev@icloud.com>
2024-02-06 12:31:08 +0200

    Homework 9 Arrays

--- js/app.js
+++ js/app.js
@@ -1,39 +1,18 @@
-//Task 1 Hello User
-
-let name = prompt('What is your name?')
-alert(`Hello, ${name}! How are you?`)
-
-//Task 2 Lines and five-digit number
-
-let firstLine = prompt('Hello, please write down first line')
-let secondLine = prompt('Please write down second line')
-let thirdLine = prompt('Please write down third line')
-alert('Result of concat: ' + firstLine + secondLine + thirdLine)
-
-let number = prompt('Hello, please write down a five-digit number')
-alert(number.charAt(0) + ' ' + number.charAt(1) + ' ' + number.charAt(2) + ' ' + number.charAt(3) + ' ' + number.charAt(4)) //or we can use dividing 10 and operator %
-
-//Task 3 Calculator
-
-let operation = prompt('Hello, I am a super fast Calculator! Please chose operation  (add, sub, mult, div)')
-let firstNumb = prompt('Great! Write down a first number')
-let secondNumb = prompt('Cool! Write down a second number')
-if (operation === 'add') {
-    alert('Sum is: ' + (parseInt(firstNumb) + parseInt(secondNumb)))
-} else if (operation === 'sub') {
-    alert('Sub is: ' + (parseInt(firstNumb) - parseInt(secondNumb)))
-
-} else if (operation === 'mult') {
-    alert('Mult is: ' + (parseInt(firstNumb) * parseInt(secondNumb)))
-} else if (operation === 'div') {
-    alert('Div is: ' + (parseInt(firstNumb) / parseInt(secondNumb)))
+const array = [];
+let size = parseInt(prompt("Enter array length"));
+if (!isNaN(size)) {
+    for (let i = 0; i < size; i++) {
+        let value = prompt('Enter value #' + i);
+        array.push(value);
+    }
+    alert("Your array is " + array);
+    alert("Sorted array is " + array.sort());
+    if (array.length <= 5) {
+        array.splice(2, 5);
+        alert("Spliced array is " + array);
+    } else {
+        alert("The size of the array to small, we can not delete elements from 2 to 4. Please enter array with min length 5");
+    }
 } else {
-    alert('Unsupported operation, please reload page and chose a correct operation (add, sub, mult, div)')
+    alert("Please enter correct number and try again");
 }
-
-//Task 4 Hours in seconds
-
-let hours = prompt('Hey, it calculator from hours to second! Put humber of hours')
-const seconds = hours * 60 * 60
-alert(hours + ` hours equal - ${seconds} sec`)
-
