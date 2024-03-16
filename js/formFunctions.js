864ee1645b0084bfaf6812e913b9a7c821f1793f
Danylo Yenbaiev <ennnbaev@icloud.com>
2024-03-16 20:32:14 +0200

    Home task 24 added delete operation

--- js/formFunctions.js
+++ js/formFunctions.js
@@ -1,109 +1,115 @@
 document.querySelector('.save_btn').addEventListener('click', event => {
-  const id = event.target.getAttribute('data-id');
-  const user = collectData();
-  const isValid = validateUser(user);
+    const id = event.target.getAttribute('data-id');
+    const user = collectData();
+    const isValid = validateUser(user);
 
-  if (!isValid) {
-    alert('enter data');
-  } else {
-    if (!id) {
-      user.id = generateUserId();
-      saveUser(user);
-
-      const parent = document.querySelector('#grid');
-      const userRow = createUserRow(user);
-      parent.appendChild(userRow);
+    if (!isValid) {
+        alert('enter data');
     } else {
-      user.id = id;
-      const userIndex = getUserIndexById(id);
-      if (userIndex === -1) {
-        return;
-      }
+        if (!id) {
+            user.id = generateUserId();
+            saveUser(user);
 
-      users[userIndex] = user;
+            const parent = document.querySelector('#grid');
+            const userRow = createUserRow(user);
+            parent.appendChild(userRow);
+        } else {
+            user.id = id;
+            const userIndex = getUserIndexById(id);
+            if (userIndex === -1) {
+                return;
+            }
 
-      const userRow = document.querySelector(`.row[data-id="${id}"]`);
-      userRow.innerHTML = '';
-      createUserRowContent(userRow, user);
-      saveUser(user, false);
-      
+            users[userIndex] = user;
+
+            const userRow = document.querySelector(`.row[data-id="${id}"]`);
+            userRow.innerHTML = '';
+            createUserRowContent(userRow, user);
+            saveUser(user, false);
+
+        }
+
+        clearFormData();
     }
-    
-    clearFormData();
-  }
 
-  // for(let item of form) {
-  //   if (item.type !== 'button') {
-  //     console.log(item.name, item.value);
-  //   }
-  // }
-  
-  document.querySelector('#form').classList.add('hidden');
+    // for(let item of form) {
+    //   if (item.type !== 'button') {
+    //     console.log(item.name, item.value);
+    //   }
+    // }
+
+    document.querySelector('#form').classList.add('hidden');
 });
 
 function clearFormData() {
-  const formElements = document.forms[0].elements;
-  for (let item of formElements) {
-    if (item.type === 'button') {
-      item.removeAttribute('data-id');
-    } else {
-      item.value = '';
+    const formElements = document.forms[0].elements;
+    for (let item of formElements) {
+        if (item.type === 'button') {
+            item.removeAttribute('data-id');
+        } else {
+            item.value = '';
+        }
     }
-  }
 }
 
+function deleteUserData(user) {
+    users.splice(getUserIndexById(user.id), 1);
+    localStorage.setItem('users', JSON.stringify(users));
+    const userRow = document.querySelector(`.row[data-id="${user.id}"]`);
+    userRow.innerHTML = '';
+}
 
 function editUserData(user) {
-  document.querySelector('#form').classList.remove('hidden');
+    document.querySelector('#form').classList.remove('hidden');
 
-  const formElements = document.forms[0].elements;
-  formElements.save_btn.setAttribute('data-id', user.id);
+    const formElements = document.forms[0].elements;
+    formElements.save_btn.setAttribute('data-id', user.id);
 
-  for (let key in user) {
-    if (key === 'id') {
-      continue;
+    for (let key in user) {
+        if (key === 'id') {
+            continue;
+        }
+        formElements[key].value = user[key];
     }
-    formElements[key].value = user[key];
-  }
 }
 
 function collectData() {
-  const form = document.forms[0].elements;
-  const name = form.name.value;
-  const login = form.login.value;
-  const age = form.age.value;
-  const email = form.email.value;
+    const form = document.forms[0].elements;
+    const name = form.name.value;
+    const login = form.login.value;
+    const age = form.age.value;
+    const email = form.email.value;
 
-  const user = {
-    name,
-    login,
-    age,
-    email,
-  };
+    const user = {
+        name,
+        login,
+        age,
+        email,
+    };
 
-  return user;
+    return user;
 }
 
 function validateUser(user) {
-  let isValid = true;
+    let isValid = true;
 
-  for(let value in user) {
-    if (user[value] === '') {
-      isValid = false;
-      break;
+    for (let value in user) {
+        if (user[value] === '') {
+            isValid = false;
+            break;
+        }
     }
-  }
 
-  return isValid;
+    return isValid;
 }
 
 function generateUserId() {
-  return Date.now().toString();
+    return Date.now().toString();
 }
 
 function saveUser(user, isNew = true) {
-  if (isNew) {
-    users.push(user);
-  }
-  localStorage.setItem('users', JSON.stringify(users));
+    if (isNew) {
+        users.push(user);
+    }
+    localStorage.setItem('users', JSON.stringify(users));
 }
--- js/functions.js
+++ js/functions.js
@@ -1,90 +1,107 @@
 function showUsers(usersList) {
-  const parent = document.querySelector('#grid');
-  parent.addEventListener('click', gridClickHandler);
+    const parent = document.querySelector('#grid');
+    parent.addEventListener('click', gridClickHandler);
 
-  // dataset data-* (data-attributes)
+    // dataset data-* (data-attributes)
 
-  usersList.forEach(user => {
-    const userRow = createUserRow(user);
-    parent.appendChild(userRow);
-  });
+    usersList.forEach(user => {
+        const userRow = createUserRow(user);
+        parent.appendChild(userRow);
+    });
 }
 
 function createUserRow(user) {
-  const userRow = document.createElement('div');
-  userRow.classList.add('user_row');
-  userRow.classList.add('row');
-  userRow.setAttribute('data-id', user.id);
+    const userRow = document.createElement('div');
+    userRow.classList.add('user_row');
+    userRow.classList.add('row');
+    userRow.setAttribute('data-id', user.id);
 
-  createUserRowContent(userRow, user);
+    createUserRowContent(userRow, user);
 
-  return userRow;
+    return userRow;
 }
 
 function createUserRowContent(userRow, user) {
-  createElement('div', { className: 'user_id col-sm-1' }, user.id, userRow);
-  createElement('div', { className: 'user_name col' }, user.name, userRow);
+    createElement('div', {className: 'user_id col-sm-1'}, user.id, userRow);
+    createElement('div', {className: 'user_name col'}, user.name, userRow);
 
-  const divButtons = createElement('div', { className: 'user_buttons col' }, '', userRow);
-  createElement('input', { type: 'button', value: 'View', 'data-action': 'view', className: 'btn btn-primary' }, '', divButtons);
-  createElement('input', { type: 'button', value: 'Edit', 'data-action': 'edit', className: 'btn btn-warning' }, '', divButtons);
-  createElement('input', { type: 'button', value: 'Delete', 'data-action': 'delete', className: 'btn btn-danger' }, '', divButtons);
+    const divButtons = createElement('div', {className: 'user_buttons col'}, '', userRow);
+    createElement('input', {
+        type: 'button',
+        value: 'View',
+        'data-action': 'view',
+        className: 'btn btn-primary'
+    }, '', divButtons);
+    createElement('input', {
+        type: 'button',
+        value: 'Edit',
+        'data-action': 'edit',
+        className: 'btn btn-warning'
+    }, '', divButtons);
+    createElement('input', {
+        type: 'button',
+        value: 'Delete',
+        'data-action': 'delete',
+        className: 'btn btn-danger'
+    }, '', divButtons);
 }
 
 
 // attributes: { value: 'Delete', type: 'button', className: 'delete_btn' }
 // eventHandlers: { click: () => {}, mouseover: () => {}... }
 function createElement(tagName, attributes, content, parent, eventHandlers) {
-  const element = document.createElement(tagName);
+    const element = document.createElement(tagName);
 
-  for (let key in attributes) {
-    const attribute = key === 'className' ? 'class' : key;
-    element.setAttribute(attribute, attributes[key]);
-  }
+    for (let key in attributes) {
+        const attribute = key === 'className' ? 'class' : key;
+        element.setAttribute(attribute, attributes[key]);
+    }
 
-  for (let event in eventHandlers) {
-    element.addEventListener(event, eventHandlers[event]);
-  }
+    for (let event in eventHandlers) {
+        element.addEventListener(event, eventHandlers[event]);
+    }
 
-  element.textContent = content;
-  parent.appendChild(element);
+    element.textContent = content;
+    parent.appendChild(element);
 
-  return element;
+    return element;
 }
 
 function gridClickHandler(event) {
-  if (event.target.nodeName === 'INPUT') {
-    const dataAction = event.target.getAttribute('data-action');
-    // const userId = event.target.parentNode.parentNode.getAttribute('data-id');
-    const userId = event.target.closest('.user_row').getAttribute('data-id');
+    if (event.target.nodeName === 'INPUT') {
+        const dataAction = event.target.getAttribute('data-action');
+        // const userId = event.target.parentNode.parentNode.getAttribute('data-id');
+        const userId = event.target.closest('.user_row').getAttribute('data-id');
 
-    const user = getUserById(userId);
+        const user = getUserById(userId);
 
-    switch (dataAction) {
-      case 'view':
-        showUserData(user);
-        break;
-      case 'edit':
-        editUserData(user);
-        break;
-      case 'delete':
-        alert("DELETE button" + userId);
-        break;
+        switch (dataAction) {
+            case 'view':
+                showUserData(user);
+                break;
+            case 'edit':
+                editUserData(user);
+                break;
+            case 'delete':
+                if (confirm("Do you want to delete this account with id: " + userId)) {
+                    deleteUserData(user);
+                }
+                break;
+        }
     }
-  }
 }
 
 function getUserById(id) {
-  return users.find(user => user.id === id);
+    return users.find(user => user.id === id);
 }
 
 function getUserIndexById(id) {
-  return users.findIndex(user => user.id === id);
+    return users.findIndex(user => user.id === id);
 }
 
 function showUserData(user) {
-  document.querySelector('#view').classList.remove('hidden');
-  document.querySelector('#view').innerHTML = `
+    document.querySelector('#view').classList.remove('hidden');
+    document.querySelector('#view').innerHTML = `
     <div>
       <p>Name: ${user.name}</p>
       <p>Login: ${user.login}</p>
@@ -94,8 +111,8 @@ function showUserData(user) {
     </div>
   `;
 
-  document.querySelector('.view_close_button').addEventListener('click', () => {
-    document.querySelector('#view').classList.add('hidden');
-    document.querySelector('#view').innerHTML = '';
-  });
+    document.querySelector('.view_close_button').addEventListener('click', () => {
+        document.querySelector('#view').classList.add('hidden');
+        document.querySelector('#view').innerHTML = '';
+    });
 }
